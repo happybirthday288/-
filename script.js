c0e545bc854a7b66d1caf0c475d68af066167574
happybirthday288 <siva1geituhgi@gmail.com>
2024-10-04 13:05:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const photos = document.querySelectorAll('.photo');
+    
+    // Initialize AOS (Animate On Scroll Library)
+    AOS.init({
+        duration: 1000,
+    });
+});
